dcc73a95bc0628d16ac4f089e74312b509a0276c
Mark Derricutt <mark@talios.com>
2011-04-22 14:22:39 +1200

    Updated integration test to start a server, and make requests, testing returned content.

---
+++ restbuilder-maven-plugin/src/it/restbuilder/src/main/java/com/theoryinpractise/restbuilder/it/ApplicationBuilder.java
@@ -0,0 +1,100 @@
+package com.theoryinpractise.restbuilder.it;
+
+import com.google.inject.Binder;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import org.restlet.*;
+import org.restlet.data.Protocol;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Resource;
+
+public class ApplicationBuilder {
+
+    Router router = new Router();
+    private Module module;
+    private Component component = null;
+
+    public ApplicationBuilder(Module module) {
+        this.module = module;
+    }
+
+    public ApplicationBuilder attach(String path, Class<? extends Resource> resourceClass) {
+        router.attach(path, new GuiceFinder(resourceClass));
+        return this;
+    }
+
+    public Application buildApplication() {
+        return new Application() {
+            @Override
+            public Restlet createRoot() {
+                return router;
+            }
+        };
+    }
+
+
+    public void startServer(final int port) {
+        if (component != null) {
+            throw new UnsupportedOperationException("Server is already running");
+        }
+
+        try {
+            // Create a new Component.
+            component = new Component();
+
+            // Add a new HTTP server listening on port 8182.
+            component.getServers().add(Protocol.HTTP, port);
+
+            // Attach the sample application.
+            component.getDefaultHost().attach(buildApplication());
+
+            // Start the component.
+            component.start();
+        } catch (Exception e) {
+            // Something is wrong.
+            e.printStackTrace();
+        }
+
+    }
+
+    public void stopServer() {
+        if (component == null) {
+            throw new UnsupportedOperationException("No server started");
+        }
+
+        try {
+            component.stop();
+            component = null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+
+    private class GuiceFinder extends Finder {
+
+        private Class<? extends Resource> resourceClass;
+
+        public GuiceFinder(Class<? extends Resource> resourceClass) {
+            this.resourceClass = resourceClass;
+        }
+
+        public Resource createTarget(final Request request, final Response response) {
+            Injector injector = Guice.createInjector(module, new Module() {
+                public void configure(Binder binder) {
+                    binder.bind(Request.class).toInstance(request);
+                    binder.bind(Response.class).toInstance(response);
+                }
+            });
+
+            return injector.getInstance(resourceClass);
+
+        }
+
+    }
+
+}
---
+++ restbuilder-maven-plugin/src/it/restbuilder/src/test/java/com/theoryinpractise/restbuilder/it/TestApplication.java
@@ -0,0 +1,63 @@
+package com.theoryinpractise.restbuilder.it;
+
+import com.example.rbuilder.handler.AccountHandler;
+import com.example.rbuilder.resource.AccountResource;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class TestApplication {
+
+
+    private ApplicationBuilder server;
+
+    @BeforeMethod
+    public void startServer() {
+        Module module = new Module() {
+            public void configure(Binder binder) {
+                binder.bind(AccountHandler.class).to(TestAccountHandler.class);
+            }
+        };
+
+        server = new ApplicationBuilder(module)
+                .attach("/account", AccountResource.class);
+
+        server.startServer(8182);
+
+    }
+
+    @AfterMethod
+    public void stopServer() {
+        server.stopServer();
+    }
+
+
+    @Test
+    public void testApplication() throws InterruptedException, ExecutionException, IOException {
+
+        Map account = requestJsonAsMap("http://localhost:8182/account");
+        assertThat(account.get("displayName")).isEqualTo("Test Account");
+
+    }
+
+    private Map requestJsonAsMap(final String url) throws IOException, InterruptedException, ExecutionException {
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        Future<Response> f = asyncHttpClient.prepareGet(url).execute();
+        Response r = f.get();
+        return new ObjectMapper().readValue(r.getResponseBody(), Map.class);
+    }
+
+
+}
